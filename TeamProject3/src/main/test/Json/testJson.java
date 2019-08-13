ce1a69e794012a7eb8637f55bb8ee65b0bc85e81
오대근 <odg920302@gmail.com>
2019-08-13 14:36:09 +0900

    프로젝트 준비

--- TeamProject3/src/main/test/Json/testJson.java
+++ TeamProject3/src/main/test/Json/testJson.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import main.Json.JsonService;
@@ -13,14 +14,20 @@ import main.Json.JsonService;
 public class testJson implements JsonService{
 
 	@Override
-	public JsonObject execute(HttpServletRequest request, HttpServletResponse response)
+	public Object execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		JsonObject test = new JsonObject();
+		JsonObject test = new JsonObject(), test2 = new JsonObject(), result = new JsonObject();
+		JsonArray list = new JsonArray();
 		
-		test.addProperty("test1", 1);
-		test.addProperty("test2", true);
-		test.addProperty("test3", "3");
+		test.addProperty("lat", 37.63163577);
+		test.addProperty("lng", 127.0419499);
+		list.add(test);
 		
-		return test;
+		test2.addProperty("lat", 37.63163577);
+		test2.addProperty("lng", 127.0419499);
+		list.add(test2);
+		
+		result.add("positions", list);
+		return result;
 	}
 }
